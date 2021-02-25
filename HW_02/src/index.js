aea59de66a4bcc28c15a588ab672f8f9143a6ed3
ShDA <shda1615@gmail.com>
2021-02-25 16:19:39 +0300

    HW_02

---
+++ HW_02/src/index.js
@@ -0,0 +1,12 @@
+//import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import App from '../src/components/App/App.jsx'
+//import { App } from '../src/components/App/App.jsx'
+
+import {App} from './components/App'
+
+//ReactDOM.render(React.createElement(App),document.querySelector('#root'));
+
+//вызов через JSX
+ReactDOM.render(<App />,document.querySelector('#root'));
